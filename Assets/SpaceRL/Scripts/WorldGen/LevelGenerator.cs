feec482ad75b5263bfd608ef9f74ba92adda3c73
Daniel Moulson <dan24797@gmail.com>
2020-01-05 19:11:07 +0000

    initial setup and folder structure

---
+++ Assets/SpaceRL/Scripts/WorldGen/LevelGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    //Publics
+    public enum Level{
+        PlaceholderEasy,
+        PlaceholderMed,
+        PlaceholderHard
+    }
+
+    //Privates
+    private RoomBuilder roomBuilder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateLevel(Level level)
+    {
+
+    }
+}
---
+++ Assets/SpaceRL/Scripts/WorldGen/RoomBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomBuilder : MonoBehaviour
+{
+    public enum RoomType
+    {
+        
+        Standard = 0,
+        Treasure = 1,
+        Chance = 2,
+        BossRoom = 3,
+        BossTreasure = 4,
+        Shop = 5,
+        Secret = 6
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void BuildRoom(RoomType type)
+    {
+        switch (type)
+        {
+            case RoomType.BossRoom:
+                //Check if a BossRoom has already been rendered in the room
+                break;
+            case RoomType.Standard:
+                //Should be fine, no limit on standard rooms
+                break;
+            case RoomType.Shop:
+                //Probably only want one shop on each floor
+                break;
+            case RoomType.Chance:
+                //Max one per floor, sometimes not even?
+                break;
+            case RoomType.BossTreasure:
+                //Only one and can only be touching the boss room
+                break;
+            case RoomType.Treasure:
+                //Only one per floor
+                break;
+            case RoomType.Secret:
+                //No door but also no obstacles
+                break;
+        }
+    }
+}
