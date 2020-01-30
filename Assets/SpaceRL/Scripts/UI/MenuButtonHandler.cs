fefc98f85d1292c3b6c7c67e05183c7d395361b7
Daniel Moulson <dan24797@gmail.com>
2020-01-30 23:36:57 +0000

    added readable settings config

---
+++ Assets/SpaceRL/Scripts/UI/MenuButtonHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonHandler : MonoBehaviour
+{
+    public string newGameScene;
+    public GameObject mainMenuPanel;
+    public GameObject settingsMenuPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //show main menu and hide the rest
+        mainMenuPanel.SetActive(true);
+        settingsMenuPanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnNewGame()
+    {
+        SceneManager.LoadScene(newGameScene);
+    }
+
+    public void OpenSettingsMenu()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsMenuPanel.SetActive(true);
+        Settings s = new Settings();
+        List<Setting> settings = s.GetAll();
+        foreach(var setting in settings)
+        {
+            Debug.Log($"setting: {setting.Name}, value {setting.Value}");
+        }
+        
+    }
+
+    public void CloseSettingsMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        settingsMenuPanel.SetActive(false);
+    }
+}
---
+++ Assets/SpaceRL/Scripts/UI/Settings.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+
+public class Setting
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+
+
+}
+
+public static class SettingName
+{
+    public const string MoveForward = "move_forward";
+    public const string MoveBack = "move_backward";
+    public const string MoveLeft = "move_left";
+    public const string MoveRight = "move_right";
+    public const string Jump = "jump";
+    public const string PrimaryAttack = "attack1";
+}
+
+public class Settings
+{
+    private List<Setting> settings = new List<Setting>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    List<Setting> ReadSettings()
+    {
+        string dataPath = $"{System.Environment.GetFolderPath(System.Environment.SpecialFolder.ApplicationData)}/SpaceRL/";
+        string settingsPath = $"{dataPath}settings.cfg";
+        if (!Directory.Exists(dataPath)) {
+            Directory.CreateDirectory(dataPath);
+        }
+        if (!File.Exists(settingsPath))
+        {
+            File.Create(settingsPath);
+        }
+
+        //Read the settings file to a list
+        using (StreamReader sr = new StreamReader(settingsPath))
+        {
+            string line;
+            while((line = sr.ReadLine()) != null)
+            {
+                Setting s = new Setting();
+                string[] sa = line.Split('=');
+                if(sa.Length != 2)
+                {
+                    Exception CorruptFileException = new Exception();
+                    throw CorruptFileException;
+                }
+                s.Name = sa[0].Trim();
+                s.Value = sa[1].Trim();
+                if (!settings.Contains(s))
+                {
+                    settings.Add(s);
+                }
+            }
+        }
+        return settings;
+    }
+
+    public List<Setting> GetAll()
+    {
+        return ReadSettings();
+    }
+
+    public Setting Get(string setting)
+    {
+        if (settings.Count == 0) settings = ReadSettings();
+        return settings.FirstOrDefault(x => x.Name == setting);
+    }
+
+}
